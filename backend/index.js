a342fa942acf6a167cf86534106585c111a87ba0
atulj4723 <atulj9537@gmail.com>
2025-07-26 15:08:16 +0530

    backend added

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("ChatApp backend is running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
